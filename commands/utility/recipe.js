1ec9742565bac528e287231efb7a5a0910b2102a
labood230 <labood230@gmail.com>
2024-10-19 20:44:21 -0400

    removed time thing bc annoying and stupid.

--- commands/utility/recipe.js
+++ commands/utility/recipe.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const fetch = require('node-fetch');
 const config = require('../../config.json');
 
@@ -15,7 +15,7 @@ module.exports = {
                 .setDescription("Enter the unique ID for the recipe you're searching for")
                 .setRequired(false)), 
     
-    async execute(interaction) {
+    async execute(interaction){
         const ingredients = interaction.options.getString('ingredients');
         const recipeId = interaction.options.getInteger('id');
 
@@ -24,9 +24,9 @@ module.exports = {
             return;
         }
         
-        if (recipeId) {
+        if (recipeId){
             const recipeDetails = await getRecipeDetails(recipeId);
-            if (!recipeDetails) {
+            if (!recipeDetails){
                 await interaction.reply('No details found for the given recipe ID');
                 return;
             }
@@ -39,69 +39,43 @@ module.exports = {
                 .setTimestamp();
             
             await interaction.reply({ embeds: [embed] });
-        } else if (ingredients) {
+        } 
+        else if (ingredients){
             await interaction.reply(`Searching for recipes with: ${ingredients}`);
-            const timeOptions = new StringSelectMenuBuilder()
-                .setCustomId('time_filter')
-                .setPlaceholder('Select cooking time')
-                .addOptions([
-                    {
-                        label: '30 minutes and under',
-                        value: '30',
-                    },
-                    {
-                        label: '1-3 hours',
-                        value: '180',
-                    },
-                    {
-                        label: 'Overnight meals',
-                        value: '1440',
-                    },
-                ]);
+            const recipes = await getRecipes(ingredients);
+            const limitedRecipes = recipes.slice(0, 5);
 
-            const row = new ActionRowBuilder().addComponents(timeOptions);
+            if (limitedRecipes.length > 0){
+                const embed = embedDisplay(limitedRecipes[0], 1, limitedRecipes.length);
+                const row = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('prev')
+                            .setLabel('Previous')
+                            .setStyle(ButtonStyle.Primary)
+                            .setDisabled(true),
+                        new ButtonBuilder()
+                            .setCustomId('next')
+                            .setLabel('Next')
+                            .setStyle(ButtonStyle.Primary)
+                    );
 
-            await interaction.followUp({ content: 'Please select a cooking time:', components: [row] });
-
-            
-            const filter = i => i.customId === 'time_filter' && i.user.id === interaction.user.id;
-            const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
-
-            collector.on('collect', async i => {
-                const maxReadyTime = i.values[0];
-                const recipes = await getRecipes(ingredients, maxReadyTime);
-                const limitedRecipes = recipes.slice(0, 5);
-
-                if (limitedRecipes.length > 0) {
-                    const embed = embedDisplay(limitedRecipes[0], 1, limitedRecipes.length);
-                    const navRow = new ActionRowBuilder()
-                        .addComponents(
-                            new ButtonBuilder()
-                                .setCustomId('prev')
-                                .setLabel('Previous')
-                                .setStyle(ButtonStyle.Primary)
-                                .setDisabled(true),
-                            new ButtonBuilder()
-                                .setCustomId('next')
-                                .setLabel('Next')
-                                .setStyle(ButtonStyle.Primary)
-                        );
-
-                    await i.update({ content: `Here are the recipes under ${maxReadyTime} minutes:`, embeds: [embed], components: [navRow] });
-                    pageNav(i, limitedRecipes);
-                } else {
-                    await i.update({ content: 'No recipes found with the given ingredients for that time.', embeds: [], components: [] });
-                }
-                collector.stop();
-            });
-        } else {
+                await interaction.followUp({ embeds: [embed], components: [row] });
+                pageNav(interaction, limitedRecipes);
+            } 
+            else{
+                await interaction.followUp('No recipes were found with the given ingredients');
+            }
+        } 
+        else{
             await interaction.reply('Please provide either ingredients or a recipe ID.');
         }
     },
 };
 
+
 // embed it so prettier than just plain text //
-function embedDisplay(recipe, currentIndex, totalRecipes) {
+function embedDisplay(recipe, currentIndex, totalRecipes){
     const ingredientsList = recipe.ingredients && recipe.ingredients.length > 0 
         ? recipe.ingredients.join(', ') : 'Ingredients data not available';
 
@@ -113,21 +87,23 @@ function embedDisplay(recipe, currentIndex, totalRecipes) {
     return embed;
 }
 
+
 // page navigation w/ buttons //
-async function pageNav(interaction, recipes) {
+async function pageNav(interaction, recipes){
     let currentIndex = 0;
     const filter = i => ['prev', 'next'].includes(i.customId) && i.user.id === interaction.user.id;
     const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
 
     collector.on('collect', async i => {
-        if (i.customId === 'next') {
+        if (i.customId === 'next'){
             currentIndex = Math.min(currentIndex + 1, recipes.length - 1);
-        } else if (i.customId === 'prev') {
+        } 
+        else if (i.customId === 'prev'){
             currentIndex = Math.max(currentIndex - 1, 0);
         }
 
         const embed = embedDisplay(recipes[currentIndex], currentIndex + 1, recipes.length);
-        const navRow = new ActionRowBuilder()
+        const row = new ActionRowBuilder()
             .addComponents(
                 new ButtonBuilder()
                     .setCustomId('prev')
@@ -140,33 +116,20 @@ async function pageNav(interaction, recipes) {
                     .setStyle(ButtonStyle.Primary)
                     .setDisabled(currentIndex === recipes.length - 1)
             );
-
-        await i.update({ embeds: [embed], components: [navRow] });
+        await i.update({ embeds: [embed], components: [row] });
     });
 }
 
+
 // use Spoonacular API to get recipes based on ingreds and prep/cook time
-async function getRecipes(ingredients, maxReadyTime) {
+async function getRecipes(ingredients){
     const apiKey = config.apiKey; 
-    const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=5&ranking=1&maxReadyTime=${maxReadyTime}&ignorePantry=true&apiKey=${apiKey}`;
+    const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=5&ranking=1&ignorePantry=true&apiKey=${apiKey}`;
 
-    try {
+    try{
         const response = await fetch(url);
-        
-        if (!response.ok) {
-            const errorData = await response.json();
-            console.error('API error:', errorData);
-            return [];
-        }
-
         const recipes = await response.json();
 
-        if (!Array.isArray(recipes)) {
-            console.error('Unexpected API response format:', recipes);
-            return []; // Return an empty array if recipes is not an array
-        }
-
-        // recipe details via ID //
         const detailedRecipes = await Promise.all(recipes.map(async recipe => {
             const recipeId = recipe.id;
             const detailUrl = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`;
@@ -177,30 +140,32 @@ async function getRecipes(ingredients, maxReadyTime) {
                 title: detailData.title,
                 image: detailData.image,
                 ingredients: detailData.extendedIngredients.map(ing => ing.name),
-                id: detailData.id
+                id: detailData.id // Include the ID for the embed
             };
         }));
 
         return detailedRecipes;
-    } catch (error) {
+    } 
+    catch (error){
         console.error('Error fetching recipes:', error);
         return [];
     }
 }
 
 // recipe info //
-async function getRecipeDetails(recipeId) {
+async function getRecipeDetails(recipeId){
     const apiKey = config.apiKey;
     const url = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`;
 
-    try {
+    try{
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error(`API error: ${response.statusText}`);
         }
         const recipe = await response.json();
         return recipe;
-    } catch (error) {
+    } 
+    catch (error){
         console.error('Error fetching recipe details:', error);
         return null;
     }
