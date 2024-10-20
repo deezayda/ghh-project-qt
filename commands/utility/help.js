0daf2fb156746d3a54e8786e783cfc65dc8d9ab6
labood230 <labood230@gmail.com>
2024-10-20 12:59:03 -0400

    slfh

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,36 +1,3 @@
-// const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-// const fs = require('fs');
-// const path = require('path');
-
-// module.exports = {
-//     data: new SlashCommandBuilder()
-//         .setName('help')
-//         .setDescription('Lists all available commands and their descriptions'),
-
-//     async execute(interaction) {
-//         const commandsPath = path.join(__dirname);
-//         const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-//         const helpEmbed = new EmbedBuilder()
-//             .setColor('#3477eb')
-//             .setTitle('Available Commands')
-//             .setDescription('Here is a list of all available commands and their descriptions:')
-//             .setTimestamp()
-//             .setFooter({ text: 'Meal Manager\'s Commands'});
-
-//         let commandList = '';
-//         for (const file of commandFiles) {
-//             const command = require(path.join(commandsPath, file));
-
-//             if (command.data && command.data.name && command.data.description) {
-//                 commandList += `**/${command.data.name}**: ${command.data.description}\n\n`;
-//             }
-//         }
-
-//         helpEmbed.setDescription(commandList);
-//         await interaction.reply({ embeds: [helpEmbed] });
-//     }
-// };
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const path = require('path');
 
